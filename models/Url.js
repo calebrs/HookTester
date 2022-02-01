214a8bc3247e564314be2274c4f52f82a167166c
Caleb Smith <smithcaleb042@gmail.com>
2022-02-01 15:20:01 -0700

    added schema file

--- models/Url.js
+++ models/Url.js
@@ -1,6 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UrlSchema = new Schema({});
+const UrlSchema = new Schema({
+  url: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "url",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  requests: [
+    {
+      method: {
+        type: String,
+      },
+      host: {
+        type: String,
+      },
+      path: {
+        type: String,
+      },
+      created: {
+        type: Date,
+      },
+      parameters: {
+        type: String,
+      },
+      headers: {
+        type: Object,
+      },
+      body: {
+        type: String,
+      },
+    },
+  ],
+});
 
 module.exports = Url = mongoose.model("url", PostSchema);
