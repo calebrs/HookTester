87be006b8ea0b600a93881ff6ccfd016037ff472
sean <sean.richardson.umd18@gmail.com>
2022-02-06 16:09:41 -0800

    Added 'copy link' button

--- client/src/components/InspectPage.js
+++ client/src/components/InspectPage.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import axios from 'axios';
 import Header from './Header';
 import RequestList from './RequestList';
+import CopyButton from './CopyButton';
 import { baseURL, COLORS } from '../constants.js';
 
 const InspectPage = () => {
@@ -19,10 +20,15 @@ const InspectPage = () => {
     fetchRequests();
   }, []);
 
+  const binUrl = `${baseURL}/r/${url}`;
+
   return (
     <Container>
       <Header />
-      <Info><h3>Your new URL is: {baseURL}/r/{url}</h3></Info>
+      <Info>
+        <h3>Your new URL is: {binUrl}</h3>
+        <CopyButton toCopy={binUrl} />
+      </Info>
       <RequestList requests={requests} />
     </Container>
   );
